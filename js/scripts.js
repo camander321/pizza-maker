79cc8da1c44f90f6447475d06dd0f81d5547d875
cameronanderson94 <cameronander94@gmail.com>
2018-01-26 10:58:17 -0800

    add css styling and gather user input

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,30 @@ function Topping(name, price) {
 
 function Pizza() {
 	this.toppings = Array(0);
+	this.size = "md";
 	
 	Pizza.prototype.addTopping = function(topping) {
 		this.toppings.push(topping);
 	}
 	
+	Pizza.prototype.clearToppings = function() {
+		this.toppings = Array(0);
+	}
+	
 	Pizza.prototype.getPrice = function() {
 		var price = 0;
+		switch(this.size) {
+			case "sm":
+				price += 8;
+				break;
+			case "md":
+				price += 12;
+				break;
+			case "lg":
+				price += 15;
+
+		}
+		
 		this.toppings.forEach(function(t){
 			price += t.price;
 		});
@@ -21,20 +38,49 @@ function Pizza() {
 }
 
 function getToppings() {
-	console.log("not implemented");
+	var toppings = Array(0);
+	
+	toppings.push(new Topping("Extra cheese", 1));
+	toppings.push(new Topping("Pepperoni", 0.5));
+	toppings.push(new Topping("Mushrooms", 0.5));
+	toppings.push(new Topping("Onions", 0.5));
+	toppings.push(new Topping("Sausage", 1));
+	toppings.push(new Topping("Bacon", 1));
+	toppings.push(new Topping("Black olives", 0.25));
+	toppings.push(new Topping("Green peppers", 0.5));
+	toppings.push(new Topping("Pineapple", 0.25));
+	toppings.push(new Topping("Spinach", 0.25));
+	toppings.push(new Topping("Anchovies", 1));
+	
+	return toppings;
 }
 
 function showToppings(toppings) {
-	console.log("not implemented");
+	var list = $("#topping-list");
+	toppings.forEach(function(t, i) {
+		var box = "<input type='checkbox' name='topping' id='t" + i + "' value='" + i + "'>"
+		var label = "<label for='t" + i + "'>" + box + t.name + "</label><br>";
+		var div = "<div class='col-md-3'>" + label + "</div>";
+		list.append(div);
+	});
 }
 
 
+
+
 $(document).ready(function() {
-	showToppings(getToppings())
+	var toppings = getToppings();
+	showToppings(toppings);
 	var pizza = new Pizza();
-	pizza.addTopping(new Topping("cheese", 1));
-	pizza.addTopping(new Topping("anchovis", 2));
 	
-	console.log(pizza.getPrice());
-	
+	$("#form").submit(function(event) {
+		event.preventDefault()
+		pizza.clearToppings();
+		pizza.size = $("#size").val();
+		$("input:checkbox[name=topping]:checked").each(function(){
+			var idx = parseInt($(this).val());
+			pizza.addTopping(toppings[idx]);
+		});
+		console.log(pizza);
+	});
 });
